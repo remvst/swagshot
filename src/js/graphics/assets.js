bae76a4a679c681e462ffc79c5c7f23ac11be536
remvst <remvst@gmail.com>
2019-08-22 23:17:06 -0400

    Gut unused things

--- src/js/graphics/assets.js
+++ src/js/graphics/assets.js
@@ -1,14 +1,3 @@
-renderMatrix = (matrix, ctx, colorGetter) => {
-    for (let row = 0 ; row < matrix.length ; row++) {
-        for (let col = 0 ; col < matrix[0].length ; col++) {
-            if (matrix[row][col]) {
-                ctx.fillStyle = toColor(colorGetter(matrix[row][col]));
-                ctx.fillRect(col, row, 1, 1);
-            }
-        }
-    }
-};
-
 pixelate = (can, pixelSize) => {
     const smaller = createCanvas(can.width / pixelSize, can.height / pixelSize, (ctx, smallCan) => {
         ctx.imageSmoothingEnabled = false;
