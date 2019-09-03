0b966b28438c7d81101e4cc5cf98719378241ec9
Rémi Vansteelandt <remvst@gmail.com>
2019-09-03 13:32:28 +0200

    Group globals

--- src/js/globals.js
+++ src/js/globals.js
@@ -2,18 +2,12 @@ let R, // canvas context
     G, // Game instance
     W, // World instance
     P, // Player instance
-    w = window;
-
-
-let onMenu = true,
-    isMouseDown;
-
-let MOUSE_SENSITIVITY = parseFloat(localStorage[1]) || 0.5;
-let POINTER_LOCKED;
-let ON_PLAY_BUTTON;
-
-let CANVAS = nomangle(g);
-
-const DAMAGE_ANGLES = [];
-
-let RESET_BACKGROUND_TIMEOUT;
+    w = window,
+    onMenu = true,
+    isMouseDown,
+    MOUSE_SENSITIVITY = parseFloat(localStorage[1]) || 0.5,
+    POINTER_LOCKED,
+    ON_PLAY_BUTTON,
+    CANVAS = nomangle(g),
+    DAMAGE_ANGLES = [],
+    RESET_BACKGROUND_TIMEOUT;
