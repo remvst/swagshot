9aec89df576f67f27f8e016e2cb72cf7f463742f
remvst <remvst@gmail.com>
2019-09-08 08:47:16 -0400

    Tiny adjsutments

--- src/js/render/render-flare.js
+++ src/js/render/render-flare.js
@@ -9,13 +9,13 @@ renderFlare = () => {
         const starX = castIndex * CANVAS_WIDTH / SLICE_COUNT;
         const distFromCenterX = evaluate(CANVAS_WIDTH / 2) - starX;
         const distFromCenterY = evaluate(CANVAS_HEIGHT / 2 - STAR_Y) - lookupOffset();
-        const alphaRatio = 1 - limit(0, distP(0, 0, distFromCenterX, distFromCenterY) / 300, 1);
+        const alphaRatio = 1 - limit(0, distP(0, 0, distFromCenterX, distFromCenterY) / 250, 1);
 
-        for (let ratio = 0 ; ratio < 1 ; ratio += 0.2) {
-            const size = ratio ? (ratio * 7 % 1) * 50 : 100;
+        for (let ratio = 0 ; ratio < 1 ; ratio += 0.15) {
+            const size = ratio ? (ratio * 56 % 1) * 25 : 50;
             R.globalAlpha = ratio ? alphaRatio * 0.5 : 1;
             drawImage(
-                ratio ? halo('#ff0') : halo('#ff0'),
+                halo('#ff0'),
                 starX + 2 * ratio * distFromCenterX - size / 2,
                 STAR_Y + 2 * ratio * distFromCenterY - size / 2,
                 size,
