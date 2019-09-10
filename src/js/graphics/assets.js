23c578a18c696ad51a45c0305c3bd7d293924b0d
remvst <remvst@gmail.com>
2019-09-10 19:11:46 -0400

    Making it fit again

--- src/js/graphics/assets.js
+++ src/js/graphics/assets.js
@@ -1,12 +1,12 @@
 pixelate = (can, pixelSize) => {
     const smaller = createCanvas(can.width / pixelSize, can.height / pixelSize, (ctx, smallCan) => {
         ctx.imageSmoothingEnabled = false;
-        ctx.drawImage(can, 0, 0, can.width, can.height, 0, 0, smallCan.width, smallCan.height);
+        ctx.drawImage(can, 0, 0, smallCan.width, smallCan.height);
     });
 
     return createCanvas(can.width, can.height, (ctx, largeCan) => {
         ctx.imageSmoothingEnabled = false;
-        ctx.drawImage(smaller, 0, 0, smaller.width, smaller.height, 0, 0, largeCan.width, largeCan.height);
+        ctx.drawImage(smaller, 0, 0, largeCan.width, largeCan.height);
     });
 };
 
